83997d42f37d9ea4e07b602d543aa49e0b5d48ab
Henry Havens <hhavens@hotmail.com>
2020-07-24 06:22:43 -0500

    new

---
+++ Views/Structs_Enums.swift
@@ -0,0 +1,16 @@
+//
+//  Structs_Enums.swift
+//  SwiftUIVacationPlanner
+//
+//  Created by Henry Havens on 7/22/20.
+//  Copyright © 2020 Henry Havens. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+enum enumContinent {
+    case europe, asia, northAmerica, southAmerica, africa
+}
+
+
